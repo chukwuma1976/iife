43720b4d84994e192cfb8eba9db37a560c71d45c
Chukwuma Anyadike <chukwuma.anyadike@gmail.com>
2025-01-20 07:51:38 -0600

    minor change to iifeFactorial

--- iifeFactorial.js
+++ iifeFactorial.js
@@ -12,3 +12,4 @@ console.log('2! = ', factorial(2));
 console.log('3! = ', factorial(3));
 console.log('4! = ', factorial(4));
 console.log('5! = ', factorial(5));
+console.log('10! = ', factorial(10));
